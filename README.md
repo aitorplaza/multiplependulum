776e9a28ab455dedb0f248396a1096db75133bff
Aitor <aitorp@gmail.com>
2019-07-31 11:35:01 +0200

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # multiplependulum
+
+Animated plot made with MATLAB. Ten uncoupled simple pendulums of different lengths many pendulums of different lengths swing at the same time producing visual wave patterns. 
+
+Pendulums length have been adjusted to synchronize again when the shortest pendulum has done 40 oscillations. 
+
+
+Here you can see the result:
+
+![](PendulumWaves.gif)
--- mpendulum.m
+++ mpendulum.m
@@ -3,65 +3,70 @@ clc
 
 %% Define constants
 
-N=9; 
-% dl=0.01;
-% l=0.2;
-% lengths = l:dl:l+dl*(N-1)
-lengths = [0.205, 0.218, 0.232, 0.248, 0.266, 0.285 ,0.306, 0.330, 0.357];
+nPend=10; % Number of pendulums
+nOscillations = 40% Number of oscilations to synchronize again
+
+lengths = ComputeLengths(nPend,nOscillations)
+
 theta_0 = 0.3;
 dtheta_0 = 0;
 
-t_final=30.0;
-delta_t=0.01;
+t_final=nOscillations+0.24;
+delta_t=0.04;
 
 %% Integrate
 
 data.l = lengths';
 data.g = 9.8;
 
-state_init = [theta_0*ones(1,N),dtheta_0*ones(1,N)]';
+state_init = [theta_0*ones(1,nPend),dtheta_0*ones(1,nPend)]';
 tspan = [0:delta_t:t_final];
 
 options = odeset('RelTol',1e-7);
 [tSeries, stateSeries] = ode45 (@deriv, tspan, state_init, options, data);
 
+%plot(stateSeries(:,1:nPend))
+
 %% Plot
-clrs=['r','g','b','c','m','y','k'];
-xSeries =  sin(stateSeries(:,1:N))*diag(lengths);
-ySeries = -cos(stateSeries(:,1:N))*diag(lengths);
+figure(1)
+plot(stateSeries(:,1:nPend));
 
+xSeries =  sin(stateSeries(:,1:nPend))*diag(lengths);
+ySeries = -cos(stateSeries(:,1:nPend))*diag(lengths);
 
-h=figure(1)
+h=figure(2)
 hold on;axis equal;grid on; 
-title('Multiple Pendulum','interpreter','latex','Fontsize', 12) 
+title('Pendulum Waves','interpreter','latex','Fontsize', 18) 
 yMin=min(min(ySeries));
 xMax=max(max(xSeries));
-axis([-1.1*xMax 1.1*xMax yMin*1.1 0.1])
+axis([-1.2*xMax 1.2*xMax yMin*1.2 0.02])
 set(gca,'xtick',[])
 set(gca,'ytick',[])
+hAxes = gca;     %Axis handle
+hAxes.XRuler.Axle.LineStyle = 'none';  
+hAxes.YRuler.Axle.LineStyle = 'none';
 
-for i= N:-1:1
+for i= 1:1:nPend  
     bar(i) = plot([0,xSeries(1,i)],[0,ySeries(1,i)],'k');
     ball(i)= scatter(xSeries(1,i),ySeries(1,i),100,'filled' );
 end
+plot(0,0,'.k');
 hold off
 
 % animate plot:
 disp('Press a key to continue!')  
 pause;
-filename = 'multiPendulum.gif';
+filename = 'PendulumWaves.gif';
 
-dt=delta_t*0.5;
+dt=2*delta_t;
 for i=1:length(tSeries)
-    for j= N:-1:1
+    for j= 1:1:nPend
         x = xSeries(i,j);
         y = ySeries(i,j);
         set(bar(j),'Xdata',[0, x], 'YData',[0,y])
         set(ball(j),'Xdata', x, 'YData',y)
     end
-    
-    
-    pause(dt)
+    tSeries(i)
     
     frame = getframe(h);
     im = frame2im(frame);
